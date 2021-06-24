8b0549b055b916989137e31ffb16c2ae72f199db
Stirli <d.v.stanski@outlook.com>
2021-06-24 19:22:56 +0300

    refactor + fixes

--- parser/static/js/upload.js
+++ parser/static/js/upload.js
@@ -5,40 +5,32 @@ let input = document.querySelector('input');
 document.addEventListener('dragover', ev => ev.preventDefault());
 document.addEventListener('drop', ev => ev.preventDefault());
 
-dropZone.addEventListener('drop', ev =>{
+
+dropZone.addEventListener('drop', ev => {
   ev.preventDefault();
-
-  let fileList = ev.dataTransfer.files;
-  let event = 'drop';
-
-  add2Form(fileList, event)
-});
-
-dropZone.addEventListener('click', () => {
-
-  input.click();
-
-  input.addEventListener('change', () => {
-    let fileList = input.files;
-    add2Form(fileList);
-  });
-
-});
-
-let add2Form = (fileList, event) =>{
-
-  document.querySelector('input[type="file"]').files = fileList;
-
-  if(event == 'drop'){
-    let event = new Event('change');
-    let elem = document.querySelector('input');
-    elem.dispatchEvent(event);
+  let result = checkFilesType(ev.dataTransfer.files);
+  if (result.sucsess) {
+    let fileList = ev.dataTransfer.files;
+    input.files = fileList;
+    send();
+    return;
   }
 
-};
+  alert(result.message);
+
+});
 
 
-let send = () =>{
+dropZone.addEventListener('click', () => {
+  input.click();
+});
+
+input.addEventListener('change', () => {
+  send();
+});
+
+
+let send = () => {
 
   let formData = new FormData(document.forms.file);
   let xhr = new XMLHttpRequest();
@@ -46,7 +38,7 @@ let send = () =>{
   xhr.open("POST", '/upload');
   xhr.send(formData);
 
-  xhr.onreadystatechange = function(){
+  xhr.onreadystatechange = function () {
 
     if (xhr.response == 'file upload') {
       document.location.href = '/viewing';
@@ -54,36 +46,17 @@ let send = () =>{
   }
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let send = (formData) =>{
-
-  // добавим ещё одно поле
-
-  // отправим данные
-  let xhr = new XMLHttpRequest();
-  xhr.open('POST', '/upload');
-  xhr.send(formData);
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    //перенаправить на какой-нибудь url
-    console.log('успех');
-  } else {
-    console.log('');
+function checkFilesType(files) {
+  for (const file of files) {
+    if (file.type != 'application/vnd.ms-excel') {
+      return {
+        message: `Wrong type for '${file.name} (${file.type})!'`,
+        sucsess: false
+      };
+    }
   }
-
-}*/
-
-//xhr.onload = () => alert(xhr.response);
+  return {
+    message: 'Sucsess',
+    sucsess: true
+  };
+}
